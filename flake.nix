fe543f24c2676d5fc2cad41de8720dd576078a7a
hf-ikea <bluehairguyiscool@gmail.com>
2025-01-11 17:00:05 -0800

    a

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { nixpkgs, home-manager, hyprland, ... }@inputs: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+	specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/default/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.emi = import ./users/emi.nix;
+          }
+          {
+	    programs.hyprland = {
+	      enable = true;
+              xwayland.enable = true;
+	      package = hyprland.packages."${pkgs.system}".hyprland;
+	    };
+	  }
+        ];
+      };
+  };
+}
