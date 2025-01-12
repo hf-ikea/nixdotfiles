3081c3e6f6092734475acb1fd44eb84766396e4d
hf-ikea <bluehairguyiscool@gmail.com>
2025-01-11 19:02:39 -0800

    31 current  2025-01-11 19:02:37  25.05.20250108.bffc22e     6.6.69                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -96,6 +96,11 @@
     alejandra
   ];
 
+  fonts.packages = with pkgs; [
+    font-awesome
+    jetbrains-mono
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
