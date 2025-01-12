aed5b4e287dc5efe2ac390927ca9b8eedf828cfe
hf-ikea <bluehairguyiscool@gmail.com>
2025-01-11 18:32:24 -0800

    26 current  2025-01-11 18:32:21  25.05.20250108.bffc22e     6.6.69                          *

--- users/emi.nix
+++ users/emi.nix
@@ -59,50 +59,50 @@
     settings = {
       general = {
         gaps_in = 5;
-	gaps_out = 10;
-	border_size = 2;
-	"col.active_border" = "rgb(ff5bf0) rgb(ffffff) rgb(33ccff) 45deg";
-	"col.inactive_border" = "rgb(595959)";
-	resize_on_border = false;
-	allow_tearing = false;
-	layout = dwindle;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgb(ff5bf0) rgb(ffffff) rgb(33ccff) 45deg";
+        "col.inactive_border" = "rgb(595959)";
+        resize_on_border = false;
+        allow_tearing = false;
+        layout = "dwindle";
       };
 
       "$mod" = "LALT";
       "$terminal" = "alacritty";
       "$menu" = "wofi --show drun";
-      "$snip" = "hyprshot --clipboard-only -m region"
+      "$snip" = "hyprshot --clipboard-only -m region";
       bind = [
         "$mod, Q, killactive,"
         "$mod, Return, exec, $terminal"
         "$mod SHIFT, p, exit,"
-	"$mod, Space, exec, $menu"
-	"$mod, N, exec, $snip"
-	"$mod, 1, workspace, 1"
-	"$mod, 2, workspace, 2"
-	"$mod, 3, workspace, 3"
-	"$mod, 4, workspace, 4"
-	"$mod, 5, workspace, 5"
-	"$mod, 6, workspace, 6"
-	"$mod, 7, workspace, 7"
-	"$mod, 8, workspace, 8"
-	"$mod, 9, workspace, 9"
-	"$mod, 0, workspace, 10"
+        "$mod, Space, exec, $menu"
+        "$mod, N, exec, $snip"
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
 
-	"$mod SHIFT, 1, movetoworkspace, 1"
-	"$mod SHIFT, 2, movetoworkspace, 2"
-	"$mod SHIFT, 3, movetoworkspace, 3"
-	"$mod SHIFT, 4, movetoworkspace, 4"
-	"$mod SHIFT, 5, movetoworkspace, 5"
-	"$mod SHIFT, 6, movetoworkspace, 6"
-	"$mod SHIFT, 7, movetoworkspace, 7"
-	"$mod SHIFT, 8, movetoworkspace, 8"
-	"$mod SHIFT, 9, movetoworkspace, 9"
-	"$mod SHIFT, 0, movetoworkspace, 10"
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
       ];
       bindm = [
         "$mod, mouse:272, movewindow"
-	"$mod, mouse:273, resizewindow"
+        "$mod, mouse:273, resizewindow"
       ];
     };
   };
