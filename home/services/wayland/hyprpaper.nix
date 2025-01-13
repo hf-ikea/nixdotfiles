e8e5df33384d06f23f529b4c1486197cba44620d
hf-ikea <bluehairguyiscool@gmail.com>
2025-01-12 20:46:19 -0800

    83 current  2025-01-12 20:46:15  25.05.20250108.bffc22e     6.6.69                          *

--- home/services/wayland/hyprpaper.nix
+++ home/services/wayland/hyprpaper.nix
@@ -11,8 +11,15 @@ in {
     enable = true;
     package = inputs.hyprpaper.packages.${pkgs.system}.default;
     settings = {
-      preload = [(builtins.toString wallpaper)];
-      wallpapers = [",${builtins.toString wallpaper}"];
+      ipc = false;
+      #splash = false;
+      preload = [
+        (builtins.toString wallpaper)
+      ];
+
+      wallpaper = [
+        ",${builtins.toString wallpaper}"
+      ];
     };
   };
 
