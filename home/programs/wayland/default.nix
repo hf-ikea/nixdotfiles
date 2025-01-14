20390fb1058cbb0ae57f5f0d0dcfb90000b4a755
hf-ikea <bluehairguyiscool@gmail.com>
2025-01-13 17:44:04 -0800

    98 current  2025-01-13 17:43:59  25.05.20250108.bffc22e     6.6.69                          *

--- home/programs/wayland/default.nix
+++ home/programs/wayland/default.nix
@@ -22,6 +22,13 @@
 
   xdg.portal = {
     enable = true;
+    config = {
+      common = {
+        default = [
+          "wayland"
+        ];
+      };
+    };
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
     ];
