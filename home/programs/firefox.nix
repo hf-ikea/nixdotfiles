e5d62ef4de5e27f5dd4d5c036735393cb995e44c
hf-ikea <bluehairguyiscool@gmail.com>
2025-01-14 19:29:07 -0800

    122 current  2025-01-14 19:29:02  25.05.20250108.bffc22e     6.6.69                          *

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -47,6 +47,11 @@ in {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/firefox-color/latest.xpi";
           installation_mode = "force_installed";
         };
+        "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = {
+          #stylus
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/styl-us/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
   };
