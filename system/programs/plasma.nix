ae63812d8bb1dc1033cdc68bff8131776f67b583
hf-ikea <bluehairguyiscool@gmail.com>
2025-03-01 19:08:39 -0800

    135 current  2025-03-01 19:08:31  25.05.20250108.bffc22e     6.6.69                          *

--- system/programs/plasma.nix
+++ system/programs/plasma.nix
@@ -5,4 +5,10 @@
 }: {
   services.xserver.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  systemd.user.services.set-wallpaper = {
+    description = "Set KDE Plasma wallpaper";
+    serviceConfig.ExecStart = ["/run/current-system/sw/bin/plasma-apply-wallpaperimage ~/nixos/emipaper.jpg"];
+    wantedBy = ["graphical-session.target"];
+  };
 }
