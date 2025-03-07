493d299a8bf4bcf494357661758e1087e61e9aaf
hf-ikea <bluehairguyiscool@gmail.com>
2025-03-06 21:53:59 -0800

    140 current  2025-03-06 21:53:50  25.05.20250108.bffc22e     6.6.69                          *

--- system/services/pipewire.nix
+++ system/services/pipewire.nix
@@ -8,6 +8,15 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    extraConfig.pipewire.adjust-sample-rate = {
+      "context.properties" = {
+        "default.clock.rate" = 192000;
+        "default.allowed-rates" = [192000 48000 44100];
+        #"default.clock.quantum" = 32;
+        #"default.clock.min-quantum" = 32;
+        #"default.clock.max-quantum" = 32;
+      };
+    };
   };
 
   services.pulseaudio.enable = lib.mkForce false;
