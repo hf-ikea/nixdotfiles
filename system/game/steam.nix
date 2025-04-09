c5dc7d116aaf7ee7b050cd54aff5d656571d0c4b
hf-ikea <bluehairguyiscool@gmail.com>
2025-04-08 20:50:00 -0700

    153 current  2025-04-08 20:49:50  25.05.20250108.bffc22e     6.6.69                          *

--- system/game/steam.nix
+++ system/game/steam.nix
@@ -5,10 +5,12 @@
 }: {
   programs.steam = {
     enable = true;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+  programs.gamescope.enable = true;
   environment.systemPackages = [
     pkgs.lutris
     (pkgs.lutris.override {
