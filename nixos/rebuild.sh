94e2803a3d5b97b4377e6ffb7c98931428485921
hf-ikea <forma3425@gmail.com>
2025-05-21 17:49:01 -0700

    why is this here?

--- nixos/rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/run/current-system/sw/bin/sh
-set -e
-pushd /etc/nixos/nixdotfiles
-nix fmt -- **/*.nix
-#git diff -U0 '*.nix'
-sudo nixos-rebuild switch --flake ~/nixos#olympia --upgrade --show-trace
-current=$(nixos-rebuild list-generations | grep current)
-git commit -am "$current"
-popd
